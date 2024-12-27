a3809e7d58a42102a149012a60c99de120626968
shaelsien630 <shaelsien630@gmail.com>
2024-12-27 11:48:58 +0900

    Initial Commit

---
+++ Payment/PaymentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentApp.swift
+//  Payment
+//
+//  Created by 최서희 on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PaymentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
